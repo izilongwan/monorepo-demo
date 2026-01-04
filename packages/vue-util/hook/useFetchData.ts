9f81d6c10e7432e71d84afc375d3da3086780bda
izilongwan <js.nb@qq.com>
2026-01-04 22:56:04 +0800

    feat: 重构 useFetchData Hook，添加状态管理和数据获取逻辑

--- packages/vue-util/hook/useFetchData.ts
+++ packages/vue-util/hook/useFetchData.ts
@@ -1,11 +1,41 @@
-import { reactive, toRefs } from 'vue';
+import { onMounted, reactive, toRefs } from 'vue';
 
-export const useFetchData = () => {
+export interface FetchState {
+	data: any;
+	isLoading: boolean;
+	error: Error | null | unknown;
+	refetch: () => Promise<void>;
+}
+
+export const useFetchData = (fetcher: () => Promise<any>, immediate = true) => {
 	// Simulate fetching data
-	const data = reactive({
+	const state = reactive<FetchState>({
 		data: null,
 		isLoading: false,
-		error: null
+		error: null,
+		refetch: async () => {}
 	});
-	return toRefs(data);
+	let abortController = new AbortController();
+
+	const refetch = async () => {
+		try {
+			abortController.abort?.();
+			abortController = new AbortController();
+			state.isLoading = true;
+			state.data = await fetcher();
+		} catch (error) {
+			state.error = error;
+			state.data = null;
+		} finally {
+			state.isLoading = false;
+		}
+	};
+
+	onMounted(() => {
+		if (immediate) {
+			refetch();
+		}
+	});
+
+	return toRefs(state);
 };
