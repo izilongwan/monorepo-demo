687e6cd91c03c8481f23db86c8b727d8ec7b39e3
izilongwan <js.nb@qq.com>
2026-01-07 22:12:36 +0800

    feat: 更新 SkeletonCommon 组件的默认段落行数为 12

--- apps/my-react-app/src/components/SkeletonCommon.tsx
+++ apps/my-react-app/src/components/SkeletonCommon.tsx
@@ -4,7 +4,7 @@ interface SkeletonCommonProps {
 	paragraphRows?: number;
 }
 
-export default ({ paragraphRows = 6 }: SkeletonCommonProps) => (
+export default ({ paragraphRows = 12 }: SkeletonCommonProps) => (
 	<div className="tw-p-5">
 		<Skeleton paragraph={{ rows: paragraphRows }} active />
 		<Skeleton.Button size="large" className="tw-mt-2" active />
