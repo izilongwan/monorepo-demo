6ed344782f39b8999cf0265890eb1fc5d9747654
izilongwan <js.nb@qq.com>
2026-01-08 17:54:15 +0800

    fixed: 修复 NotFound 组件按钮跳转无效问题

--- apps/my-react-app/src/pages/NotFound.tsx
+++ apps/my-react-app/src/pages/NotFound.tsx
@@ -15,39 +15,42 @@ export default function NotFound() {
     console.log('NotFound routeParams:', routeParams.get('test'));
   }, [routeParams]);
 
-  function handleSetParam() {
-    const value = Number(routeParams.get('test') ?? 0) + 1;
-    routeParams.set('test', String(value));
-    setRouteParams({ ...Object.fromEntries(routeParams) });
-  }
+  function handleSetParam(e: React.MouseEvent) {
+		// 如果点击的是按钮，不执行此逻辑
+		if ((e.target as HTMLElement).closest('button')) {
+			return;
+		}
+		const value = Number(routeParams.get('test') ?? 0) + 1;
+		routeParams.set('test', String(value));
+		setRouteParams({ ...Object.fromEntries(routeParams) });
+	}
 
-  return (
-    <div className={ style.container } onClick={ handleSetParam }>
-      <Result
-        status="404"
-        title="404"
-        subTitle="抱歉，您访问的页面不存在"
-        extra={
-          showReturnBtn && <Space>
-            <Button
-              type="primary"
-              size="large"
-              icon={ <HomeOutlined /> }
-              onClick={ () => navigate('/home') }
-            >
-              回到首页
-            </Button>
-            <Button
-              size="large"
-              icon={ <ArrowLeftOutlined /> }
-              onClick={ () => navigate(-1) }
-            >
-              返回上一页
-            </Button>
-          </Space>
-        }
-
-      />
-    </div>
-  );
+	return (
+		<div className={style.container} onClick={handleSetParam}>
+			<Result
+				status="404"
+				title="404"
+				subTitle="抱歉，您访问的页面不存在"
+				extra={
+					showReturnBtn && (
+						<Space>
+							<Button
+								type="primary"
+								size="large"
+								icon={<HomeOutlined />}
+								onClick={() => navigate('/home')}>
+								回到首页
+							</Button>
+							<Button
+								size="large"
+								icon={<ArrowLeftOutlined />}
+								onClick={() => navigate(-1)}>
+								返回上一页
+							</Button>
+						</Space>
+					)
+				}
+			/>
+		</div>
+	);
 }
