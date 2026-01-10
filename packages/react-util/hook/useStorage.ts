87baaf6377d8f4a41158c76f2295966bad07e3cc
izilongwan <js.nb@qq.com>
2026-01-10 21:49:29 +0800

    feat: 优化 useStorage 钩子，支持过期时间管理和存储对象结构

--- packages/react-util/hook/useStorage.ts
+++ packages/react-util/hook/useStorage.ts
@@ -1,20 +1,36 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export function useStorage<T>(
 	key: string,
 	defaultValue?: T,
-	type = StorageType.LOCAL
+	type = StorageType.LOCAL,
+	expire: number = 0
 ) {
 	const storage =
 		type === StorageType.SESSION ? window.sessionStorage : window.localStorage;
 	const storedValue = storage.getItem(key);
-	const initialValue = storedValue ? JSON.parse(storedValue) : defaultValue;
+	const storedObj = storedValue ? JSON.parse(storedValue) : null;
+	let initialValue = storedObj?.value ?? defaultValue;
+	const isExpired = useRef(
+		storedObj && storedObj.expire > 0 && storedObj.expire < Date.now()
+	);
+
+	if (isExpired.current) {
+		storage.removeItem(key);
+		initialValue = defaultValue;
+	}
 
 	const [value, setValue] = useState<T>(initialValue);
 
 	const setStoredValue = (newValue: T) => {
 		setValue(newValue);
-		storage.setItem(key, JSON.stringify(newValue));
+		storage.setItem(
+			key,
+			JSON.stringify({
+				value: newValue,
+				expire: expire > 0 ? Date.now() + expire : 0
+			})
+		);
 	};
 
 	return [value, setStoredValue] as const;
@@ -25,8 +41,21 @@ export enum StorageType {
 	SESSION = 'session'
 }
 
-export const useLocalStorage = <T>(key: string, defaultValue?: T) =>
-	useStorage<T>(key, defaultValue, StorageType.LOCAL);
+export const useLocalStorage = <T>(
+	key: string,
+	defaultValue?: T,
+	expire: number = 0
+) => useStorage<T>(key, defaultValue, StorageType.LOCAL, expire);
 
-export const useSessionStorage = <T>(key: string, defaultValue?: T) =>
-	useStorage<T>(key, defaultValue, StorageType.SESSION);
+export const useSessionStorage = <T>(
+	key: string,
+	defaultValue?: T,
+	expire: number = 0
+) => useStorage<T>(key, defaultValue, StorageType.SESSION, expire);
+
+export enum TimeUnits {
+	SECOND = 1000,
+	MINUTE = 1000 * 60,
+	HOUR = 1000 * 60 * 60,
+	DAY = 1000 * 60 * 60 * 24
+}
